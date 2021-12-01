61a022ce113fcf9291b7ce5bef3ce89f07d93c6d
HITESH KISHOR CHAUDHARI <72800256+hitman-dev@users.noreply.github.com>
2021-12-01 23:07:49 +0530

    Delete JAVA/src/DAY_9/INTERFACE_DEMO/com/demo directory

--- JAVA/src/DAY_9/INTERFACE_DEMO/com/demo/intefaces/I3.java
+++
@@ -1,9 +0,0 @@
-package DAY_9.INTERFACE_DEMO.com.demo.intefaces;
-
-public interface I3 extends I1,I2{
-    void m31();
-    void m32();
-    static void m4() {
-    	System.out.println("in static function");
-    }
-}
